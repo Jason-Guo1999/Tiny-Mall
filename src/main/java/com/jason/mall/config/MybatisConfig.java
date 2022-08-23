973fd51a9dc93da5634b2539ecdd395a42e76159
Jason Guo <zhichen_guo@163.com>
2022-08-23 11:38:42 -0400

    Revert "update Order admin"
    This reverts commit 570ba6a2733c6de7bf1eb6ba64b8af49da9d0a04.

--- src/main/java/com/jason/mall/config/MybatisConfig.java
+++ src/main/java/com/jason/mall/config/MybatisConfig.java
@@ -2,10 +2,8 @@ package com.jason.mall.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 @MapperScan({"com.jason.mall.dao","com.jason.mall.mbg.mapper"})
 public class MybatisConfig {
 
